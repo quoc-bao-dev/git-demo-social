80198de532fede1b46dc2da3c93ba05fcd4559a9
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2024-11-29 17:14:53 +0700

    fea: setup router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-const App = () => {
-    console.log(1111);
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-    return <div>App</div>;
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/feed">Feed</Link>
+            <Routes>
+                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/login" element={<h1>Login</h1>} />
+                <Route path="/feed" element={<h1>Feed</h1>} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
